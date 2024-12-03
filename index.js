3192b914bc151cc41770a27891c5ff30684ce1ad
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-04 00:32:29 +0600

    String concat method

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // string CharAt method
 var country="Bangladesh"
     console.log(
-        country.charAt(5)
+        //country.charAt(5)
+)
+
+//string concat method
+var country1='Bangladesh'
+var country2='India'
+console.log(
+    country1.concat(country2)
 )
