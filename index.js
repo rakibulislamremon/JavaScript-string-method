2082e5678a74e31426e664a5d62bd95aead22ab8
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-04 18:52:03 +0600

    added string indexof and lastindexof

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ var country="Bangladesh"
 )
 
 //string concat method
-var country1='Bangladesh'
+/*var country1='Bangladesh'
 var country2='India'
 console.log(
     country1.concat(country2)
+)*/
+
+//String IndexOf method
+/*var country="Bangladesh";
+console.log(
+    country.indexOf("l")
+)*/
+
+//String lastindex of method
+var country="Bangladesh";
+console.log(
+    country.lastIndexOf("h")
 )
